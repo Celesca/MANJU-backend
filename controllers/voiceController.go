a24ca16b7bd54fb8fb61f5750616a91629656c73
celesca <folk.sawit@gmail.com>
2025-11-18 02:15:12 +0700

    modified: voices

--- controllers/voiceController.go
+++ controllers/voiceController.go
@@ -1,36 +1,36 @@
 package controllers
 
 import (
-    "manju/backend/repository"
-    "manju/backend/services"
+	"manju/backend/repository"
+	"manju/backend/services"
 
-    "github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2"
 )
 
-type VoiceController struct{
-    repo *repository.VoiceRepository
+type VoiceController struct {
+	repo *repository.VoiceRepository
 }
 
 func NewVoiceController(repo *repository.VoiceRepository) *VoiceController {
-    return &VoiceController{repo: repo}
+	return &VoiceController{repo: repo}
 }
 
 func (vc *VoiceController) CreateVoice(c *fiber.Ctx) error {
-    return services.CreateVoice(c, vc.repo)
+	return services.CreateVoice(c, vc.repo)
 }
 
 func (vc *VoiceController) ListVoices(c *fiber.Ctx) error {
-    return services.ListVoices(c, vc.repo)
+	return services.ListVoices(c, vc.repo)
 }
 
 func (vc *VoiceController) ListVoicesByUser(c *fiber.Ctx) error {
-    return services.ListVoicesByUser(c, vc.repo)
+	return services.ListVoicesByUser(c, vc.repo)
 }
 
 func (vc *VoiceController) GetVoice(c *fiber.Ctx) error {
-    return services.GetVoice(c, vc.repo)
+	return services.GetVoice(c, vc.repo)
 }
 
 func (vc *VoiceController) DeleteVoice(c *fiber.Ctx) error {
-    return services.DeleteVoice(c, vc.repo)
+	return services.DeleteVoice(c, vc.repo)
 }
