b113a8b8aef12f5717796f755b2f29a9fa25e8b6
celesca <folk.sawit@gmail.com>
2025-11-17 20:13:19 +0700

    add: docs

--- main.go
+++ main.go
@@ -4,14 +4,18 @@ import (
 	"log"
 	"manju/backend/config/database"
 
-	"github.com/gofiber/fiber/v2"
 	routes "manju/backend/routes"
+
+	"github.com/gofiber/contrib/swagger"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	database.Connect()
 	app := fiber.New()
 
+	app.Use(swagger.New())
 	api := app.Group("/api/v1")
 	api.Get("/health", func(c *fiber.Ctx) error {
 		return c.SendString("OK")
